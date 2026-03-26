294ef2afcbe210cadee424e88d3460dc1b6fd5fa
abhi5hekS <singabhi825@gmail.com>
2026-03-26 11:45:43 +0530

    Add Home route to the application

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import CreatePost from './pages/CreatePost'
 import Feed from './pages/Feed.jsx'
+import Home from './pages/Home.jsx'
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/create-post' element={< CreatePost />} />
         <Route path='/feed' element={< Feed />} />
       </Routes>
